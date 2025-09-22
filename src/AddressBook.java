4ca751f537ab3ebf5de86425e6d3da6702db6f27
Anita Gaffuri Kasbiy <anitakasbiy@gmail.com>
2025-09-22 12:10:49 -0400

    committing

---
+++ src/AddressBook.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+    private ArrayList<BuddyInfo> buddies = new ArrayList<>();
+
+    public void addBuddy(BuddyInfo buddy) {
+        if(buddy != null) {
+            buddies.add(buddy);
+        }
+    }
+
+    public BuddyInfo removeBuddy(int index) {
+        if(index >= 0 && index < buddies.size()) {
+            buddies.remove(index);
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Address Book");
+        AddressBook addressBook = new AddressBook();
+        BuddyInfo buddyInfo = new BuddyInfo("Tom",  "Carleton", "613");
+        addressBook.addBuddy(buddyInfo);
+        addressBook.removeBuddy(0);
+    }
+}
---
+++ src/BuddyInfo.java
@@ -0,0 +1,25 @@
+public class BuddyInfo {
+    private String name;
+    private String address;
+    private String phoneNumber;
+
+    public BuddyInfo(String name, String address, String phoneNumber){
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() { return name; }
+    public String getAddress() { return address; }
+    public String getPhoneNumber() { return phoneNumber; }
+
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        BuddyInfo buddyInfo = new BuddyInfo("Homer", "123 Happy St.", "(123)-456-7890");
+        System.out.println(buddyInfo.name);
+
+    }
+
+}
